c4d23a0fb6edc998a9d2da3678319dc4ecd49432
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-02-17 09:51:30 -0800

    app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,26 @@
 import React from 'react'
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      this.state = {
+        todos: [
+          {
+            task: 'Organize Garage',
+            id: 1528817077286,
+            completed: false
+          },
+          {
+            task: 'Bake Cookies',
+            id: 1528817084358,
+            completed: false
+          }
+        ]
+      }
+    }
+  }
+}
 export default class App extends React.Component {
   render() {
     return (
